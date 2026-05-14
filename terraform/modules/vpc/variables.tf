7ca0e10f73bb79a38fa525a90357a887cfba7e0d
tani-mani-meow <tanishq.ingawale@gmail.com>
2026-05-14 14:32:39 +0530

    working on adjustments

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability zones to deploy into"
+  type        = list(string)
+}
